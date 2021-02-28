960fe8c09d04d1c1f3e54b4d8b0b48586f760723
Irina Park <park_irina@naver.com>
2021-03-01 05:03:59 +0900

    new commit

---
+++ src/main/java/web/model/Role.java
@@ -0,0 +1,65 @@
+package web.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "role_name", unique = true)
+    private String role;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users;
+
+    public Role() {
+
+    }
+
+    public Role(int id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public Role(int id) {
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
